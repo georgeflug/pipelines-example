d77a75360fbeedc0689c86a42be65bb1bd87f857
Richard White <richard.white@rgare.com>
2019-09-13 10:03:18 -0500

    Initial commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void simpleTest() {
+        assertEquals(true, true);
+    }
+}
